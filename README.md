10c0e00f9041b90bd4132c4be6661d4cd632ff58
VegiHarendranath33 <108779050+VegiHarendranath33@users.noreply.github.com>
2022-10-12 15:50:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# es5featuresexamples
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-hwcxfu)
---
+++ index.js
@@ -0,0 +1,229 @@
+// var marks = 75;
+// if (marks >80 )
+// {
+//   console.log('GRADE A');
+// }
+// else 
+// {
+//     console.log('GRADE B');
+// }
+
+// var marks = 40;
+//  if (marks >80 )
+//  {   console.log('GRADE A');
+//  }
+//   else if (marks > 55)
+//   {
+//     console.log('GRADE B');
+//   }
+//  else 
+//  {
+//    console.log('GRADE C');
+// }
+// for (var i =0; i<5; i++)
+// {
+//     console.log('hari');
+// }
+
+//  var output = 0;
+//  for (var i =1; i<=10; i++)
+//  {
+//    output = output + i;
+// }
+// console.log(output);
+
+// var output = 1;
+// while (output <=10)
+// {
+//   output++;
+//   console.log(output);
+// }
+
+// # what is reactjs
+
+//  reactjs is A JAVASCRIPT library  used to develop user  Interface component 
+//  Basic requirements :
+
+//  1.HTML
+//  2.c                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+//  3.Javascript (Es6)
+
+//  Fundamental concepts:
+//  Variable Declaration
+//  Data Types
+//  Arrays and Objects 
+//  Functions
+//  Conditional Statements
+//  Looping Concept
+
+
+//  Variable Declaration:
+
+
+//  playcode.io/new
+
+//  after  es5 let and const have been introduced 
+
+//  var was used to declare a variable
+//  variable declaration //
+
+//  var  name = 'hari';
+//  console.log(name);
+
+//  4 data types
+//  //number string boolean undefined
+
+//  ==============================
+//  var data = 1;
+//  // var data = 1.5;
+
+//  // var data = "hari";
+//  console.log(typeof data);  ans: number.
+//  ======================
+
+// when u are trying to store any characters enclosed in single quotes or double quotes are called as string.
+
+// boolean is used to store only 2 types of values.
+// one is true and another one is false.
+
+// true: satisfying Condition
+// false: unsatisfying Condition
+// ====================================
+// var data = true;  // t should not be capital letters if u give capital letter it says true is not defined
+// console.log(type of data)
+// ans : boolean
+// ======
+// var data;  we are defining a variable and we are not assigning any value then we get 
+// the console.log(typeof data);  as undefined.it is telling as object.
+// ====
+
+// variables 
+// data types
+
+// Arrays and objects
+// ===================
+// upto know we stored in one variable only one value
+// but in arrays we can store in one variable multiple values:
+
+// ===
+
+// var data = [1,2,3,4,5 ];
+// console.log(data);
+// Every thing in array is storing in  oth position 
+// 1 is oth index
+// based on index it occupies its position.
+// ===================================
+// console.log(data[0]);
+
+// ===
+// object is nothing but storing data in terms of name and value 
+// or key and value.
+
+// name - value
+// key  - value 
+
+// name
+// john  age=30
+
+// var data  = {
+//     name : 'HarendranathVegi';
+//     age : 44;
+//     console.log(data.age);
+// }
+
+// we are accessing  name property in  data object.
+
+// similiarly we are access age property in data object.
+
+// ================================
+
+// Functions: function is nothing but used to perform a specific task in a block of code is called function
+// ---
+// func user() {
+
+//     //block of code  we can write the action inside the function
+// }
+
+// user();
+
+// func smh() {
+//     console.log('smh members are sudarshan hari anjan')
+// }
+
+// smh();
+
+// ====
+// function user willbe executed whenever we are calling a block of code 
+
+
+// func user() {
+//     console.log('Hi Hari');
+// }
+// user();
+// =================
+
+// func hari(sal)
+// {
+//     return sal + 5;
+// }
+
+// var output = hari(2100000);
+// console.log(output);
+
+// ====
+
+// completed ones : varaibale declaration,Datatypes,Arrays and Objects Functions 
+// # conditional statements in Javascript
+// if u pay amount then only calls will go ,or like above 90 A grade and below 90 B grade like that 
+
+// conditional 
+// Based on some conditions u want to perform some operations 
+// // there are 3 conditonal operators in Javascript 
+// 1. if   ,  if-else , if-else if-else
+// var  marks  = 85;
+// if more than 2 conditions
+// fail then go for final else 
+
+// Looping concepts:
+// if u want to perform some action multiple times or based on number of conditions
+// then we go for Looping.
+// In Looping we have  // for loop and  // while loop
+// for (statement1; statement2; statement3){
+//     //block of code
+// }
+
+// for (variable_declaration(variable Initialisation),condition, increment/decrement){
+
+// }
+// once the condition fails block of code willnot be executed in while loop 
+
+
+//  All fundamental concepts completed
+
+// 1. Variable Declaration 
+// 2. Data Types 
+// 3. Arrays and  Objects
+// 4. Functions
+// 5. Conditional Statements
+// 6. Looping Concept
+
+// Es5  features:
+// 1.is Array()
+// 2. map()
+// 3. filter()
+// 4. forEach()
+// 5. every()
+// 6. some()
+// 7. indexOf()
+// 8. lastIndexOf()
+
+// JSON Features
+// stringify()
+
+// Date Features 
+// valueOf()
+
+// is Array Example
+
+var data = 1;
+console.log(Array.isArray(data));
